8d0055dc30f961382884b23ba675d2ce9185b106
miguel.herrero <miguel.herrero@cabify.com>
2020-09-27 20:32:53 +0200

    test: :white_check_mark: Test the number of posts returned

--- test/post_service_test.dart
+++ test/post_service_test.dart
@@ -1,13 +1,14 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:injectable/injectable.dart' as injectable;
+import 'package:injectable/injectable.dart' as di;
 import 'package:mockito/mockito.dart';
 
+import 'package:flutter_demo/datamodels/post.dart';
 import 'package:flutter_demo/services/api.dart';
 import 'package:flutter_demo/services/posts_service.dart';
 
 import 'di/locator.dart';
 
-@injectable.LazySingleton(as: Api)
+@di.LazySingleton(as: Api)
 class MockApi extends Mock implements Api {}
 
 void main() {
@@ -23,12 +24,17 @@ void main() {
       expect(postService != null, true);
     });
 
-    test('Given user 1, should call api with user 1', () async {
+    test('Given user 1, should call api with user 1 and return 10 posts',
+        () async {
       final mockApi = locator<Api>();
       var postService = PostsService(mockApi);
 
-      await postService.getPostsForUser(1);
+      when(mockApi.getPostsForUser(1)).thenAnswer(
+          (_) => Future.value(List<Post>.generate(10, (index) => Post())));
+
+      var posts = await postService.getPostsForUser(1);
       verify(mockApi.getPostsForUser(1));
+      expect(posts.length, 10);
     });
   });
 }
